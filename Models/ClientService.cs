6a32e0f015ad0a1f07766ac22c08d0eb234a0ee8
Emily Richardson <richar35@mail.nmc.edu>
2020-11-22 10:42:30 -0500

    Add project files.

---
+++ Models/ClientService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPages.Models
+{
+    public class ClientService
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [ForeignKey("ClietID")]
+        public int ClientID{ get; set; }
+        public Client Client { get; set; }
+
+        [ForeignKey("ServicesID")]
+        public int ServicesID{ get; set; }
+        public Services Services { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
+    }
+}
